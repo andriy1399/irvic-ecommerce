59c6e90e76411c83c0cc8849e7a6309be977a1c8
andriy1399 <andrijn333@gmail.com>
2020-12-21 18:04:30 +0200

    added basic i18n

--- irvik-client/src/app/app.module.ts
+++ irvik-client/src/app/app.module.ts
@@ -1,7 +1,11 @@
+// Core modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components and pages
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -10,11 +14,22 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { BagComponent } from './pages/bag/bag.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
 import { StoreComponent } from './pages/store/store.component';
-import { MatRippleModule } from '@angular/material/core';
 import { ScrollBtnComponent } from './components/scroll-btn/scroll-btn.component';
+
+// Angular materials modules
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+
+// Other libraries
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { LangDirective } from './shared/directives/lang.directive';
+
+// root functions
+export function HttpLoaderFactory(http: HttpClient): any {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -27,14 +42,24 @@ import { ScrollBtnComponent } from './components/scroll-btn/scroll-btn.component
     BagComponent,
     FavoritesComponent,
     StoreComponent,
-    ScrollBtnComponent
+    ScrollBtnComponent,
+    LangDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatRippleModule
+    MatRippleModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'pl'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
