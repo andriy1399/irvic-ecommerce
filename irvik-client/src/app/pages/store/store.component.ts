305c5ccb5682e0367f17144f405af542184fc986
andriy1399 <andrijn333@gmail.com>
2021-02-02 17:41:57 +0200

    fixed -  search input put in the form tag and now when we press enter calling the search method

--- irvik-client/src/app/pages/store/store.component.ts
+++ irvik-client/src/app/pages/store/store.component.ts
@@ -9,7 +9,7 @@ import { ProductTranslate } from '../../shared/models/product-translate.model';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
+  styleUrls: ['./store.component.scss'],
 })
 export class StoreComponent implements OnInit {
   categories: ICategory[] = [];
@@ -19,7 +19,7 @@ export class StoreComponent implements OnInit {
     private categoriesServ: CategoriesService,
     private translate: TranslateService,
     private productsService: ProductService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getCategories();
@@ -30,14 +30,14 @@ export class StoreComponent implements OnInit {
   }
 
   getCategories(): void {
-    this.categoriesServ.getCategories().subscribe(categories => {
+    this.categoriesServ.getCategories().subscribe((categories) => {
       this.categories = categories;
       this.categoryForTranslate(categories);
     });
   }
 
   getProducts(): void {
-    this.productsService.getProducts().subscribe(products => {
+    this.productsService.getProducts().subscribe((products) => {
       this.products = products;
       const translatedProducts = new ProductTranslate(products, this.translate);
       translatedProducts.prepareToTranslate();
@@ -48,10 +48,13 @@ export class StoreComponent implements OnInit {
     if (category) {
       category = category.split('').join('');
     }
-    this.productsService.getFullTextSearchProducts(this.searchName || category || '')
-      .subscribe(products => {
-        this.products = products;
-      });
+    if (this.searchName || category) {
+      this.productsService
+        .getFullTextSearchProducts(this.searchName || category || '')
+        .subscribe((products) => {
+          this.products = products;
+        });
+    }
   }
 
   public convertToSnakeCase(str: string): string {
@@ -59,17 +62,35 @@ export class StoreComponent implements OnInit {
   }
 
   private categoryForTranslate(data: ICategory[]): void {
-    const enCategories = new Map(data.map(v => ([[v.categoryEn.split(' ').join('_')], v.categoryEn])));
-    const ukCategories = new Map(data.map(v => ([[v.categoryEn.split(' ').join('_')], v.categoryUk])));
-    const plCategories = new Map(data.map(v => ([[v.categoryEn.split(' ').join('_')], v.categoryPl])));
-    this.translate.setTranslation('en', {
-      categories: Object.fromEntries(enCategories)
-    }, true);
-    this.translate.setTranslation('uk', {
-      categories: Object.fromEntries(ukCategories)
-    }, true);
-    this.translate.setTranslation('pl', {
-      categories: Object.fromEntries(plCategories)
-    }, true);
+    const enCategories = new Map(
+      data.map((v) => [[v.categoryEn.split(' ').join('_')], v.categoryEn])
+    );
+    const ukCategories = new Map(
+      data.map((v) => [[v.categoryEn.split(' ').join('_')], v.categoryUk])
+    );
+    const plCategories = new Map(
+      data.map((v) => [[v.categoryEn.split(' ').join('_')], v.categoryPl])
+    );
+    this.translate.setTranslation(
+      'en',
+      {
+        categories: Object.fromEntries(enCategories),
+      },
+      true
+    );
+    this.translate.setTranslation(
+      'uk',
+      {
+        categories: Object.fromEntries(ukCategories),
+      },
+      true
+    );
+    this.translate.setTranslation(
+      'pl',
+      {
+        categories: Object.fromEntries(plCategories),
+      },
+      true
+    );
   }
 }
