0464aced00bd3c9d4e1b7d567dbdfc402226106c
andriy1399 <andrijn333@gmail.com>
2021-02-26 17:53:04 +0200

    created favorites page html

--- irvik-client/src/app/pages/favorites/favorites.component.ts
+++ irvik-client/src/app/pages/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-favorites',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites.component.scss']
 })
 export class FavoritesComponent implements OnInit {
-
+  favorites: IProduct[] = [];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public parse(str: string): number {
+    if (typeof parseFloat(str) === 'number') {
+      return parseFloat(str);
+    } else {
+      return 0;
+    }
+  }
+  public deleteOrder(id: string | number | undefined): void {
+    if (id) {
+    }
+  }
+  public convertToSnakeCase(str: string): string {
+    return str.split(' ').join('_');
+  }
 }
